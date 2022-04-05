58420d3fafe75bd7737ce4d2a42e6c8b0cbf027c
ronipeltonen <97667321+ronipeltonen@users.noreply.github.com>
2022-04-05 09:00:10 +0300

    05042022

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -131,7 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
